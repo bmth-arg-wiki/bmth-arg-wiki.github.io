d0921adc0326bacbf92a5b3c301b170899bf0742
Lina <linaxanna@gmail.com>
2024-08-04 15:35:14 +0200

    implemented basic searching for content
    i'm not proud of this.
    Because some things tend to be mentioned a LOT,
    there are generally also a lot of search result,
    not ordered by relevance whatsoever.
    I am aware.
    I am annoyed.
    But this is a basic search functionality
    and I don't want
    to do any more javascript right now
    to do more than this absolute caveman sh*t so sorry.
    
    Back here after checking 134 errors and 24 warnings in Webstorm.
    Contemplating switching to a different IDE bc webstorm is just gaslighting me at this point.

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,12 +1,46 @@
 $(function () {
     var posts = [];
+
     $.get('/api/posts.json', function (data) {
+        console.log("Data loaded:", data); // Debugging: Check if data is loaded correctly
+
+        // Check the type of the data to ensure it's correctly parsed
+        console.log("Data type:", typeof data);
+
+        if (typeof data === "string") {
+            try {
+                data = JSON.parse(data); // Parse the data if it's a string
+            } catch (e) {
+                console.error("Failed to parse JSON data:", e);
+            }
+        }
+
         posts = data;
+        console.log("Posts assigned:", posts); // Debugging: Check if posts are assigned
+
+        // Bind the search functionality once the data is loaded
+        $('#search').on('keyup', function () {
+            performSearch();
+        });
+
+        $('#search-form').on('submit', function () {
+            performSearch();
+            return false; // Prevent default form submission
+        });
+
+        // Run search if there's already input in the search field
+        if ($('#search').val().length > 0) {
+            performSearch();
+        }
+    }).fail(function () {
+        console.error("Failed to load posts JSON");
     });
 
     function performSearch() {
         var keyword = $('#search').val().toLowerCase();
         var searchResult = [];
+
+        console.log("Performing search for keyword:", keyword); // Debugging: Check the keyword
 
         if (keyword.length > 0) {
             $('#search-result').show();
@@ -17,13 +51,21 @@ $(function () {
 
         for (var i = 0; i < posts.length; i++) {
             var post = posts[i];
+            var title = post.title ? post.title.toLowerCase() : '';
+            var description = post.description ? post.description.toLowerCase() : '';
+            var content = post.content ? post.content.toLowerCase() : '';
+
             if (
-                post.title.toLowerCase().indexOf(keyword) >= 0 ||
-                post.description.toLowerCase().indexOf(keyword) >= 0
+                title.indexOf(keyword) >= 0 ||
+                description.indexOf(keyword) >= 0 ||
+                content.indexOf(keyword) >= 0
             ) {
                 searchResult.push(post);
             }
         }
+
+        console.log("Search results:", searchResult); // Debugging: Check search results
+
         if (searchResult.length === 0) {
             $('#search-result').append(
                 '<div class="result-item"><div class="description">There is no search result.</div></div>'
@@ -33,21 +75,13 @@ $(function () {
                 $('#search-result').append(
                     '<a class="result-item" href="' +
                     searchResult[i].url +
-                    '"><div class="title">【' + searchResult[i].category + '】' +
+                    '"><div class="title">【' + (searchResult[i].category || 'Uncategorized') + '】' +
                     searchResult[i].title +
                     '</div><div class="description">' +
-                    searchResult[i].description +
+                    (searchResult[i].description || 'No description available.') +
                     '</div></a>'
                 );
             }
         }
     }
-
-    $('#search').on('keyup', function () {
-        performSearch();
-    });
-
-    $('#search-form').on('submit', function () {
-        performSearch();
-    });
 });
